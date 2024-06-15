ba09d1fad666efd489b32a618791c71f16628122
MikeyTheA <eyreonai@gmail.com>
2024-06-15 17:18:27 +0300

    change port

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const octokit = new Octokit({
 });
 
 (async () => {
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 80;
     const app = express();
     app.use(express.json());
 
