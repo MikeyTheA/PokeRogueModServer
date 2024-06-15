f2cf9218e19fe3efb9363a893d39c700429cc75f
MikeyTheA <eyreonai@gmail.com>
2024-06-15 17:33:43 +0300

    revert port change

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const octokit = new Octokit({
 });
 
 (async () => {
-    const PORT = process.env.PORT || 80;
+    const PORT = process.env.PORT || 3000;
     const app = express();
     app.use(express.json());
 
